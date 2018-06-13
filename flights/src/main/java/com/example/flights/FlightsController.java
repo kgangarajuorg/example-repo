dc59874b581a78dcd2cb0e4d6c911b367e07dc05
krishnaexplore <krishnagangaraju@gmail.com>
2018-06-14 02:34:35 +0530

    added flight details endpoint

--- flights/src/main/java/com/example/flights/FlightsController.java
+++ flights/src/main/java/com/example/flights/FlightsController.java
@@ -3,7 +3,10 @@ package com.example.flights;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.PathParam;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.exmple.flights.domain.Flight;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightsController {
 
 	@Value("${server.port}")
 	private String port;
 	
-	@RequestMapping("/something")
+	@GetMapping("/list")
 	public List<Flight> list(@RequestParam("origin") String origin, @RequestParam("destination") String dest){
 		
 		return Arrays.asList( new Flight("EK521", port), new Flight("EK522",port), new Flight("EK888",port));
-		
 	}
+	
+	@GetMapping("/details/{id}")
+	public Flight details(@PathParam("id") String id) {
+		return new Flight(id,port);
+	}
+	
+	
 }
