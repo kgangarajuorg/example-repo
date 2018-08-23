02996443c63b3ec8c056d31c9f4787635191b617
Krishna Gangaraju <37485969+krishnaexplore@users.noreply.github.com>
2018-08-23 17:35:13 +0530

    Add files via upload

---
+++ spring-boot2-docker/build_docker_images.sh
@@ -0,0 +1,4 @@
+docker build -t gangakrishh/springboot2:openjre8 -f Dockerfile.openjre8 .
+docker build -t gangakrishh/springboot2:openjre8env -f Dockerfile.openjre8env .
+docker build -t gangakrishh/springboot2:openjre8cgroup -f Dockerfile.openjre8cgroup .
+docker build -t gangakrishh/springboot2:openjre10 -f Dockerfile.openjre10 .
